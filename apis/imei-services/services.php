1bf74e68a656220bec535fa8f137ccad7b1029f9
Muzammil Shaikh <m8022888@gmail.com>
2026-08-27 16:37:44 +0500

    Update package API with where Condition And Insert Some Comments

--- apis/imei-services/services.php
+++ apis/imei-services/services.php
@@ -8,14 +8,18 @@ $packages = [];
 // If condition is true, $where stores GET value; otherwise all table data$where = "";
  
 $where = "";
+ 
+//please check it that value of name is send or not
+if (isset($_GET['name'])) { 
 
-if (isset($_GET['name'])) {
-
+   //if value is sending then insert into the $name varaible
     $name = $_GET['name'];
 
+    //if condition is true than insert $name value into $where variable
     $where = "WHERE PackageTitle = '$name'";
 }
-
+ 
+//If (if condition) is wrong than this query will be gave us entire values of columns PackageId and PackageTitle
 $sql = "SELECT PackageId, PackageTitle
         FROM tbl_gf_packages
         $where";
